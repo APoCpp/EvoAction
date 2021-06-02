3baab0523a99e1e606854c0dd3b27e406fb53c25
beryll1um <vitalii.babichuk@gmail.com>
2021-06-02 23:33:32 +0300

    mod(list): list implementation modified

--- EvoAction/src/main.cpp
+++ EvoAction/src/main.cpp
@@ -1,26 +1,7 @@
-#include <SFML/Graphics.hpp>
-
-#include <iostream>
 #include <system/actor.hpp>
+#include <tools/list.hpp>
 
 int main(int argc, char *argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "EvoAction");
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-        window.clear(sf::Color::Black);
-
-        // => Draw
-
-        window.display();
-    }
-
     return 0;
 }
--- EvoAction/src/tools/list.hpp
+++ EvoAction/src/tools/list.hpp
@@ -1,5 +1,5 @@
-#ifndef EVO_ACTION_TOOLS_ENTITY_LIST_HPP
-#define EVO_ACTION_TOOLS_ENTITY_LIST_HPP
+#ifndef EVO_ACTION_TOOLS_LIST_HPP
+#define EVO_ACTION_TOOLS_LIST_HPP
 
 # include <system/entity.hpp>
 
@@ -15,161 +15,51 @@ class List
 {
 public:
 
-    using value_type    = _Type;
-    using size_type     = std::size_t;
+    using value_type            = _Type;
+    using value_pointer_type    = std::shared_ptr<_Type>;
+    using list_type             = std::list<value_pointer_type>;
 
-private:
-
-    struct Node
-    {
-        template <class _Derived>
-        explicit Node(const _Derived &derived, Node *next) : value(new _Derived(derived)), next(next) { }
-
-        virtual ~Node()
-        {
-            delete value;
-        }
-
-        value_type *value;
-        Node *next;
-    };
+    using iterator_type         = typename list_type::iterator;
+    using const_iterator_type   = typename list_type::const_iterator;
 
 public:
 
-    class iterator
+    virtual ~List() = default;
+
+    iterator_type begin()
     {
-    public:
-
-        explicit iterator(Node *node) : _node(node) { }
-
-        operator bool()
-        {
-            return static_cast<bool>(_node);
-        }
-
-        iterator &operator++()
-        {
-            return _node = _node->next, *this;
-        }
-
-        const iterator &operator++() const
-        {
-            return _node = _node->next, *this;
-        }
-
-        value_type *operator->()
-        {
-            return _node->value;
-        }
-
-        const value_type *operator->() const
-        {
-            return _node->value;
-        }
-
-        value_type *operator*()
-        {
-            return _node->value;
-        }
-
-        const value_type *operator*() const
-        {
-            return _node->value;
-        }
-
-    protected:
-
-        mutable Node *_node;
-
-    };
-
-public:
-
-    List() : _head(nullptr), _size(0) { }
-
-    virtual ~List()
-    {
-        clear();
+        return _list.begin();
     }
 
-    /*!
-        \brief Getting an iterator to a head element
-        \result iterator
-    */
-    [[nodiscard]] iterator head()
+    const_iterator_type begin() const
     {
-        return iterator(_head);
+        return _list.begin();
     }
 
-    /*!
-        \brief Getting an const iterator to a head element
-        \result const iterator
-    */
-    [[nodiscard]] const iterator head() const
+    iterator_type end()
     {
-        return iterator(_head);
+        return _list.end();
     }
 
-    /*!
-        \brief Insert new element into the list
-        \param iterator
-        \see erase
-    */
+    const_iterator_type end() const
+    {
+        return _list.end();
+    }
+
     template <class _Derived>
-    iterator insert(const _Derived &derived)
+    iterator_type insert(const _Derived &value)
     {
-        return _size++, iterator(_head = new Node(derived, _head));
+        return _list.push_back(value_pointer_type(new _Derived(value))), --_list.end();
     }
 
-    /*!
-        \brief Erase element from the list
-        \param iterator
-        \see insert
-    */
-    void erase(const iterator &iterator)
+    void remove(const iterator_type &iterator)
     {
-        Node *curr = _head, *prev = nullptr;
-        while (curr)
-        {
-            if (curr->value == *iterator)
-            {
-                if (prev) prev->next = curr->next;
-                else _head = curr->next;
-                delete curr;
-                _size--;
-                break;
-            }
-            prev = curr, curr = curr->next;
-        }
-    }
-
-    /*!
-        \brief Getting size of the list
-        \result size_type
-    */
-    size_type size() const
-    {
-        return _size;
-    }
-
-    /*!
-        \brief Getting an iterator to a head element
-    */
-    void clear()
-    {
-        while (_head)
-        {
-            Node *next = _head->next;
-            delete _head;
-            _head = next;
-        }
-        _size = 0;
+        _list.erase(iterator);
     }
 
 protected:
 
-    Node *_head;
-    size_type _size;
+    list_type _list;
 
 };
 
